10fb4d9c07f14b601b745bd178e90deeffa75142
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-08-14 18:07:46 -0400

    fixed delete

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //listen for an event when the page is finished loading
 document.addEventListener('DOMContentLoaded', function(){
   let formEl = document.getElementById('add-form');
+  const parentEl = document.getElementById('all-lists')
+  const taskForm = document.getElementById('task-form')
+
   formEl.addEventListener('submit', function(){
     event.preventDefault();
 
@@ -11,40 +14,42 @@ document.addEventListener('DOMContentLoaded', function(){
     const allListsHTML = List.renderAll()
     const taskFormTemplate = list.renderForm()
 
-    const parentEl = document.getElementById('all-lists')
-    const taskForm = document.getElementById('task-form')
 
     parentEl.innerHTML = allListsHTML
     taskForm.innerHTML = taskFormTemplate
     formEl.reset();
-    let container = document.getElementById('container')
-    container.addEventListener('click', function(){
-      if (event.target.value === 'X') {
-        event.preventDefault();
-        let toDelete = List.all().find((list) => list.id === parseInt(event.target.dataset.listId))
-        toDelete.delete();
-        const allListsHTML = List.renderAll()
-        const taskFormTemplate = list.renderForm()
-        parentEl.innerHTML = allListsHTML
-        taskForm.innerHTML = taskFormTemplate
-        // debugger
-      }
-    })
+
 
     let formEm = document.getElementById('add-task');
     formEm.addEventListener('submit', function(){
       event.preventDefault();
 
       let findList = List.all().find((list) => list.id === parseInt(formEm.select_list.value))
-
       let task = new Task(findList, formEm.task_description.value, formEm.task_priority.value);
       console.log(task);
 
-      const parentEl = document.getElementById('all-lists')
       const allListsHTML = List.renderAll()
       parentEl.innerHTML = allListsHTML
       formEm.reset();
 
     })
+  })
+
+  let container = document.getElementById('container')
+  container.addEventListener('click', function(){
+    if (event.target.value === 'X') {
+      event.preventDefault();
+      let toDelete = List.all().find((list) => list.id === parseInt(event.target.dataset.listId))
+      // debugger;
+      toDelete.delete();
+
+      const allListsHTML = List.renderAll()
+      parentEl.innerHTML = allListsHTML
+      if (List.all().length > 0){
+        taskForm.innerHTML = List.all().map(list => list.renderForm())
+      } else {
+        taskForm.innerHTML = ""
+      }
+    }
   })
 })
